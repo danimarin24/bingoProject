641f1d069626cb3a965f313ef475b749f219c6a8
Dani Marín <danii04.mb@gmail.com>
2023-01-18 13:41:56 +0100

    update different methods, and fix some problems

--- src/bingo_main.java
+++ src/bingo_main.java
@@ -1,36 +1,64 @@
 import java.util.*;
 
 public class bingo_main {
-    // Colors per estilitzar la terminal
     public static final String ANSI_RESET="\u001B[0m";
-    // Bold
-    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
     public static final String RED_BOLD = "\033[1;31m";    // RED
-    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
     public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
-    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
     public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
 
-    // High Intensity backgrounds
-    public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
     public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
-    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
-    public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
-    public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
-    public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
-    public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
     public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+    public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
+    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+
 
     public static final String at = "@";
-    public static final String space = " ";
-
-    public static String[][] carton = new String[3][9];
+    public static final String hyphen = "—";
 
     public static boolean finished = false;
-    public static String errorMsg = "";
 
+
+    // Metode principal
+    public static void main(String[] args) {
+
+        game();
+
+
+
+    }
+
+    private static void game() {
+        int cardsNumber;
+        String[][][] cards;
+        int[][][] cardsColumns;
+        boolean character;
+        // PRINT WELCOME MESSAGE
+        getGameInfo();
+        do {
+            if (Util.demanarChar("Do you want to play bingo?", 's', 'n') == 's') {
+                cardsNumber = Util.demanarIntegerMinMax("Number of cards to be played: ", 1, 99);
+                cards = new String[cardsNumber][3][9];
+                cardsColumns = new int[cardsNumber][9][3];
+
+                generateAllCards(cards, cardsColumns);
+
+
+
+                getBomboNumbers(1, 90, cards, cardsColumns);
+
+            } else {
+                System.out.println("BYE!! See u soon :D");
+                finished = false;
+            }
+
+
+
+
+
+
+        } while (finished);
+    }
 
     private static void getGameInfo() {
         System.out.printf("""
@@ -44,43 +72,155 @@ public class bingo_main {
         );
     }
 
-    // Metode principal
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static void getBomboNumbers(int min, int max, String[][][] cards, int[][][] cardsColumns) {
+        int aux = 0;
+        int[] arrBomboNumbers = generateBomboArray(min, max);
+        do {
+            printCards(cards);
 
-        getGameInfo();
+            System.out.printf("%sNew number: %d%s\n", GREEN_BACKGROUND_BRIGHT + BLACK_BOLD, arrBomboNumbers[aux], ANSI_RESET);
 
-        int randomNum;
-        String stringRandomNum;
-        int cardsNumber = 25;
 
-        String[][][] cards = new String[cardsNumber][3][9];
-        int[][] cardV2 = new int[cardsNumber][27];
+            searchAndReplaceValue(arrBomboNumbers[aux], cards);
 
-        boolean checked = false;
+            // incrementar posición del nuevo número;
+            aux++;
+        }while (Util.demanarChar("Next Number", 's', 'n') == 's');
 
-        System.out.println("ejecutandose");
 
-        for (int x = 0; x < cards.length; x++) {
-            for (int y = 0; y < cards[x].length; y++) {
-                for (int z = 0; z < cards[x][y].length; z++){
-                    do {
-                        randomNum = (int) (Math.random()*90+1);
-                        stringRandomNum = convertIntToString(randomNum);
-                        checked = checkRandomNumber(stringRandomNum, cards[x]);
-                    } while (checked);
-                    cards[x][y][z] = stringRandomNum;
+    }
+
+    /**
+     * Método para buscar los números que van saliendo del bombo
+     * y remplazarlos con un guion.
+     * @param num
+     * @param arr
+     * @void Imprime si se tiene el valor buscado, y si lo ha reemplazado
+     */
+    private static void searchAndReplaceValue(int num, String[][][] arr) {
+        boolean isIn = false;
+        int position;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                isIn = Arrays.asList(arr[i][j]).contains(String.valueOf(num));
+                if (isIn) {
+                    System.out.println("You had this number, I mark it with a hyphen. " + YELLOW_BACKGROUND_BRIGHT + BLACK_BOLD + "[-]" + ANSI_RESET);
+                    position = Arrays.asList(arr[i][j]).indexOf(String.valueOf(num));
+                    arr[i][j][position] = hyphen;
+                    break;
                 }
             }
         }
+    }
+
+    /**
+     * Método para generar un arreglo de las medidas que se pasan
+     * en los parámetros que genera números aleatorios que no se
+     * repiten entre ellos.
+     * @param min
+     * @param max
+     * @return un arreglo unidimensional con números random.
+     */
+    private static int[] generateBomboArray(int min, int max) {
+        int randomNum;
+        boolean checked;
+        int[] bomboNumbers = new int[max];
+        for (int i = 0; i < max; i++) {
+            do {
+                randomNum = (int)(Math.random() * max) + min;
+                checked = checkRandomNumberArr(randomNum, bomboNumbers);
+            } while (checked);
+            bomboNumbers[i] = randomNum;
+        }
+        return bomboNumbers;
+    }
+
+    private static void generateAllCards(String[][][] cards, int[][][] cardsColumns) {
+        getRandomValues(cards, cardsColumns);
+        sortCards(cards, cardsColumns);
+        fillCardsSorted(cards, cardsColumns);
         fillCardsAt(cards);
-        printCards(cards);
-        System.out.println("ejecutandose");
+    }
+
+    private static void getRandomValues(String[][][] cards, int[][][] cardsColumns) {
+        int aux, minRange, maxRange, range, randomNum;
+        boolean checked;
+        for (int x = 0; x < cards.length; x++) {
+            for (int y = 0; y < cards[x].length; y++) {
+                aux = 1;
+                for (int z = 0; z < cards[x][y].length; z++) {
+                    maxRange = (aux < 9) ? (aux * 10 - 1) : (aux * 10);
+                    minRange = (aux == 1) ? 1 : (aux * 10 - 10);
+                    range = maxRange - minRange + 1;
+                    do {
+                        randomNum = (int)(Math.random() * range) + minRange;
+                        //System.out.printf("z = %d || min = %d || max = %d || rango = %d || random = %d \n", z, minRange, maxRange, range, randomNum);
+
+                        checked = checkRandomNumberBiArr(randomNum, cardsColumns[x]);
+                    } while (checked);
+                    cardsColumns[x][z][y] = randomNum;
+
+                    aux++;
+                }
+            }
+        }
+    }
+
+    // RELLENAR EL ARRAY DE STRINGS
+    private static void fillCardsSorted(String[][][] cards, int[][][] cardsColumns) {
+        for (int x = 0; x < cards.length; x++) {
+            for (int y = 0; y < cards[x].length; y++) {
+                for (int z = 0; z < cards[x][y].length; z++) {
+                    cards[x][y][z] = String.valueOf(cardsColumns[x][z][y]);
+                }
+            }
+        }
+
+    }
+
+    // ORDENAR LOS CARTONES
+    private static void sortCards(String[][][] cards, int[][][] cardsColumns) {
+        for (int x = 0; x < cards.length; x++) {
+            for (int y = 0; y < cards[x].length; y++) {
+                for (int z = 0; z < cards[x][y].length; z++) {
+                    Arrays.sort(cardsColumns[x][z]);
+                }
+            }
+        }
+    }
+
+    // REVISAR NUMERO REPETIDO ARRAY BIDIMENSIONAL
+    private static boolean checkRandomNumberBiArr(int num, int[][] arr) {
+        boolean repeated = false;
+
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                if (num == anInt) {
+                    repeated = true;
+                    break;
+                }
+            }
+        }
+
+        return repeated;
+    }
+
+    // REVISAR NUMERO REPETIDO ARRAY UNIDIMENSIONAL
+    private static boolean checkRandomNumberArr(int num, int[] arr) {
+        boolean repeated = false;
+
+        for (int j : arr) {
+            if (num == j) {
+                repeated = true;
+                break;
+            }
+        }
+
+        return repeated;
     }
 
     private static void fillCardsAt(String[][][] arr) {
         int counter = 0, atPosition;
-        String stringAtPosition;
 
         for (int x = 0; x < arr.length; x++) {
             for (int y = 0; y < arr[x].length; y++) {
@@ -98,36 +238,26 @@ public class bingo_main {
         }
     }
 
-    private static String convertIntToString(int num) {
-        return "" + num;
-    }
-
     private static void printCards(String[][][] arr) {
-        System.out.println(WHITE_BACKGROUND_BRIGHT + BLACK_BOLD + "CARTONES" + ANSI_RESET);
+        System.out.println();
         for (String[][] strings : arr) {
-            System.out.print(RED_BACKGROUND_BRIGHT + BLACK_BOLD);
             for (String[] string : strings) {
                 for (String s : string) {
-                    System.out.printf("%3s", s);
+                    if (s.equals(at)) {
+                        printColor(WHITE_BACKGROUND_BRIGHT, BLACK_BOLD, ANSI_RESET, s);
+                    } else if (s.equals(hyphen)) {
+                        printColor(YELLOW_BACKGROUND_BRIGHT, BLACK_BOLD, ANSI_RESET, s);
+                    } else {
+                        printColor(RED_BACKGROUND_BRIGHT, BLACK_BOLD, ANSI_RESET, s);
+                    }
                 }
                 System.out.println();
             }
-            System.out.println(ANSI_RESET);
+            System.out.println();
         }
     }
 
-    private static boolean checkRandomNumber(String num, String[][] arr) {
-        boolean repeated = false;
-
-        for (String[] string : arr) {
-            for (String s : string) {
-                if (num.equals(s)) {
-                    repeated = true;
-                    break;
-                }
-            }
-        }
-
-        return repeated;
+    private static void printColor(String bgColor, String color, String reset, String text) {
+        System.out.printf(bgColor + color + "%2s " + reset, text);
     }
 }
--- src/bingo_testing.java
+++ src/bingo_testing.java
@@ -3,10 +3,19 @@ import java.util.*;
 public class bingo_testing {
     public static final String ANSI_RESET="\u001B[0m";
     public static final String RED_BOLD = "\033[1;31m";    // RED
+    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
     public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
     public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
 
+    public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
+    public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
+    public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
+    public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
+
+
     public static final String at = "@";
+    public static final String hyphen = "—";
+
     public static boolean finished = false;
 
 
@@ -31,6 +40,11 @@ public class bingo_testing {
                 cardsNumber = Util.demanarIntegerMinMax("Number of cards to be played: ", 1, 99);
                 cards = new String[cardsNumber][3][9];
                 cardsColumns = new int[cardsNumber][9][3];
+
+                generateAllCards(cards, cardsColumns);
+
+
+
                 getBomboNumbers(1, 90, cards, cardsColumns);
 
             } else {
@@ -59,10 +73,66 @@ public class bingo_testing {
     }
 
     private static void getBomboNumbers(int min, int max, String[][][] cards, int[][][] cardsColumns) {
-        generateAllCards(cards, cardsColumns);
+        int aux = 0;
+        int[] arrBomboNumbers = generateBomboArray(min, max);
+        do {
+            printCards(cards);
+
+            System.out.printf("%sNew number: %d%s\n", GREEN_BACKGROUND_BRIGHT + BLACK_BOLD, arrBomboNumbers[aux], ANSI_RESET);
 
 
-        printCards(cards);
+            searchAndReplaceValue(arrBomboNumbers[aux], cards);
+
+            // incrementar posición del nuevo número;
+            aux++;
+        }while (Util.demanarChar("Next Number", 's', 'n') == 's');
+
+
+    }
+
+    /**
+     * Método para buscar los números que van saliendo del bombo
+     * y remplazarlos con un guion.
+     * @param num
+     * @param arr
+     * @void Imprime si se tiene el valor buscado, y si lo ha reemplazado
+     */
+    private static void searchAndReplaceValue(int num, String[][][] arr) {
+        boolean isIn = false;
+        int position;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                isIn = Arrays.asList(arr[i][j]).contains(String.valueOf(num));
+                if (isIn) {
+                    System.out.println("You had this number, I mark it with a hyphen. " + YELLOW_BACKGROUND_BRIGHT + BLACK_BOLD + "[-]" + ANSI_RESET);
+                    position = Arrays.asList(arr[i][j]).indexOf(String.valueOf(num));
+                    arr[i][j][position] = hyphen;
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Método para generar un arreglo de las medidas que se pasan
+     * en los parámetros que genera números aleatorios que no se
+     * repiten entre ellos.
+     * @param min
+     * @param max
+     * @return un arreglo unidimensional con números random.
+     */
+    private static int[] generateBomboArray(int min, int max) {
+        int randomNum;
+        boolean checked;
+        int[] bomboNumbers = new int[max];
+        for (int i = 0; i < max; i++) {
+            do {
+                randomNum = (int)(Math.random() * max) + min;
+                checked = checkRandomNumberArr(randomNum, bomboNumbers);
+            } while (checked);
+            bomboNumbers[i] = randomNum;
+        }
+        return bomboNumbers;
     }
 
     private static void generateAllCards(String[][][] cards, int[][][] cardsColumns) {
@@ -74,7 +144,7 @@ public class bingo_testing {
 
     private static void getRandomValues(String[][][] cards, int[][][] cardsColumns) {
         int aux, minRange, maxRange, range, randomNum;
-        boolean checked = false;
+        boolean checked;
         for (int x = 0; x < cards.length; x++) {
             for (int y = 0; y < cards[x].length; y++) {
                 aux = 1;
@@ -86,7 +156,7 @@ public class bingo_testing {
                         randomNum = (int)(Math.random() * range) + minRange;
                         //System.out.printf("z = %d || min = %d || max = %d || rango = %d || random = %d \n", z, minRange, maxRange, range, randomNum);
 
-                        checked = checkRandomNumberInt(randomNum, cardsColumns[x]);
+                        checked = checkRandomNumberBiArr(randomNum, cardsColumns[x]);
                     } while (checked);
                     cardsColumns[x][z][y] = randomNum;
 
@@ -119,7 +189,8 @@ public class bingo_testing {
         }
     }
 
-    private static boolean checkRandomNumberInt(int num, int[][] arr) {
+    // REVISAR NUMERO REPETIDO ARRAY BIDIMENSIONAL
+    private static boolean checkRandomNumberBiArr(int num, int[][] arr) {
         boolean repeated = false;
 
         for (int[] ints : arr) {
@@ -128,6 +199,20 @@ public class bingo_testing {
                     repeated = true;
                     break;
                 }
+            }
+        }
+
+        return repeated;
+    }
+
+    // REVISAR NUMERO REPETIDO ARRAY UNIDIMENSIONAL
+    private static boolean checkRandomNumberArr(int num, int[] arr) {
+        boolean repeated = false;
+
+        for (int j : arr) {
+            if (num == j) {
+                repeated = true;
+                break;
             }
         }
 
@@ -154,16 +239,25 @@ public class bingo_testing {
     }
 
     private static void printCards(String[][][] arr) {
-        System.out.println("CARTONES");
-        // IMPRIMIR LOS CARTONES
+        System.out.println();
         for (String[][] strings : arr) {
             for (String[] string : strings) {
                 for (String s : string) {
-                    System.out.printf("%2s ", s);
+                    if (s.equals(at)) {
+                        printColor(WHITE_BACKGROUND_BRIGHT, BLACK_BOLD, ANSI_RESET, s);
+                    } else if (s.equals(hyphen)) {
+                        printColor(YELLOW_BACKGROUND_BRIGHT, BLACK_BOLD, ANSI_RESET, s);
+                    } else {
+                        printColor(RED_BACKGROUND_BRIGHT, BLACK_BOLD, ANSI_RESET, s);
+                    }
                 }
                 System.out.println();
             }
             System.out.println();
         }
+    }
+
+    private static void printColor(String bgColor, String color, String reset, String text) {
+        System.out.printf(bgColor + color + "%2s " + reset, text);
     }
 }
